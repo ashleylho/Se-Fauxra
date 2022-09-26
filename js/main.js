06d7819a1d74cefb7cb2860d5d1600653738074f
Ashley Ho <ashley.lho@gmail.com>
2022-09-26 18:57:52 +0000

    targeted the name, price, rating, and api_featured_image properties of each product to display on the page #1

--- js/main.js
+++ js/main.js
@@ -10,5 +10,68 @@ function loadData(event) {
   for (var i = 0; i < xhr.response.length; i++) {
     var $li = document.createElement('li');
     $productList.appendChild($li);
+
+    var $img = document.createElement('img');
+    $img.setAttribute('src', xhr.response[i].api_featured_image);
+    $li.appendChild($img);
+
+    var $h4 = document.createElement('h4');
+    $h4.textContent = xhr.response[i].name;
+    $li.appendChild($h4);
+
+    var $i = document.createElement('i');
+    if (xhr.response[i].rating >= 0.5 && xhr.response[i].rating < 1) {
+      $i.className = 'fa-solid fa-star-half-stroke';
+    } else if (xhr.response[i].rating >= 1) {
+      $i.className = 'fa-solid fa-star';
+    } else {
+      $i.className = 'fa-regular fa-star';
+    }
+    $li.appendChild($i);
+
+    $i = document.createElement('i');
+    if (xhr.response[i].rating >= 1.5 && xhr.response[i].rating < 2) {
+      $i.className = 'fa-solid fa-star-half-stroke';
+    } else if (xhr.response[i].rating >= 2) {
+      $i.className = 'fa-solid fa-star';
+    } else {
+      $i.className = 'fa-regular fa-star';
+    }
+    $li.appendChild($i);
+
+    $i = document.createElement('i');
+    if (xhr.response[i].rating >= 2.5 && xhr.response[i].rating < 3) {
+      $i.className = 'fa-solid fa-star-half-stroke';
+    } else if (xhr.response[i].rating >= 3) {
+      $i.className = 'fa-solid fa-star';
+    } else {
+      $i.className = 'fa-regular fa-star';
+    }
+    $li.appendChild($i);
+
+    $i = document.createElement('i');
+    if (xhr.response[i].rating >= 3.5 && xhr.response[i].rating < 4) {
+      $i.className = 'fa-solid fa-star-half-stroke';
+    } else if (xhr.response[i].rating >= 4) {
+      $i.className = 'fa-solid fa-star';
+    } else {
+      $i.className = 'fa-regular fa-star';
+    }
+    $li.appendChild($i);
+
+    $i = document.createElement('i');
+    if (xhr.response[i].rating >= 4.5 && xhr.response[i].rating < 5) {
+      $i.className = 'fa-solid fa-star-half-stroke';
+    } else if (xhr.response[i].rating === 5) {
+      $i.className = 'fa-solid fa-star';
+    } else {
+      $i.className = 'fa-regular fa-star';
+    }
+    $li.appendChild($i);
+
+    var $p = document.createElement('p');
+    $p.textContent = '$' + xhr.response[i].price;
+    $li.appendChild($p);
+
   }
 }
